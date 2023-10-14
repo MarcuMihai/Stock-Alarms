b630391dc28ff27d1e1f4ec52354a62508f4fdab
MarcuMihai <61512384+MarcuMihai@users.noreply.github.com>
2023-10-14 20:53:02 +0300

    Initial commit

---
+++ src/main/java/application/StockAlarmsApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlarmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAlarmsApplication.class, args);
+	}
+
+}
